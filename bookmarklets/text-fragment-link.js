e8dcdd4a5726b94bbd728630dd5467be9cf74522
Philipp Tiefenbacher <wizards23+git@gmail.com>
2025-11-16 03:51:05 +0100

    Create text-fragment-link.js

---
+++ bookmarklets/text-fragment-link.js
@@ -0,0 +1,21 @@
+javascript: (function () {
+    var s = window.getSelection().toString().trim();
+    if (!s) {
+        alert("Select some text first");
+        return;
+    }
+    s = s.replace(/\s+/g, " ");
+    var u = location.href.split("#")[0] + "#:~:text=" + encodeURIComponent(s);
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(u).then(
+            function () {
+                alert("Link copied to clipboard:\n" + u);
+            },
+            function () {
+                prompt("Copy your link:", u);
+            }
+        );
+    } else {
+        prompt("Copy your link:", u);
+    }
+})();
