b347f199bc3689390dfa56f7899d791581b2aac1
Philipp Tiefenbacher <wizards23+git@gmail.com>
2025-11-16 19:44:45 +0100

    Update text-fragment-link.js

--- bookmarklets/text-fragment-link.js
+++ bookmarklets/text-fragment-link.js
@@ -1,4 +1,5 @@
 javascript: (function () {
+/* https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Fragment/Text_fragments */
     var s = window.getSelection().toString().trim();
     if (!s) {
         alert("Select some text first");
