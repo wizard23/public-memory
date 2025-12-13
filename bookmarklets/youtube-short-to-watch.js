7c6b35cf0dd89eb442686a91c6ca588e9fcebea8
Philipp Tiefenbacher <wizards23+git@gmail.com>
2025-12-13 04:55:32 +0100

    Update youtube-short-to-watch.js

--- bookmarklets/youtube-short-to-watch.js
+++ bookmarklets/youtube-short-to-watch.js
@@ -1,31 +1,22 @@
-javascript:(() => {
-  try {
-    // Parse the current page URL
-    const currentUrl = new URL(window.location.href);
+javascript:(function () { try {
+  var url = new URL(window.location.href);
 
-    // Only run on YouTube
-    if (!/^(www\.|m\.)?youtube\.com$/.test(currentUrl.hostname)) {
-      alert("This bookmarklet only works on youtube.com");
-      return;
-    }
-
-    // Check for /shorts/<videoId>
-    const match = currentUrl.pathname.match(/^\/shorts\/([^/?#]+)/);
-    if (!match) {
-      alert("This is not a YouTube Shorts URL");
-      return;
-    }
-
-    const videoId = match[1];
-
-    // Build the normal watch URL
-    const watchUrl = new URL("https://www.youtube.com/watch");
-    watchUrl.searchParams.set("v", videoId);
-
-    // Redirect to the normal YouTube player
-    window.location.href = watchUrl.toString();
-
-  } catch (error) {
-    alert("Failed to transform YouTube Shorts URL:\n" + error);
+  if (!/^(www\.|m\.)?youtube\.com$/.test(url.hostname)) {
+    alert("This only works on youtube.com");
+    return;
   }
-})();
+
+  var shortsMatch = url.pathname.match(/^\/shorts\/([^/?#]+)/);
+  if (!shortsMatch) {
+    alert("Not a YouTube Shorts URL");
+    return;
+  }
+
+  var videoId = shortsMatch[1];
+  var watchUrl = new URL("https://www.youtube.com/watch");
+  watchUrl.searchParams.set("v", videoId);
+
+  window.location.href = watchUrl.toString();
+} catch (err) {
+  alert("Failed to convert Shorts URL:\n" + err);
+}})();
